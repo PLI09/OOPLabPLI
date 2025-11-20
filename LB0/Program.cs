264845dfb7711d2a3b40afc8864c03a352461bfe
lip4@tpu.ru <podop@LAPTOP-3T5CBGKC>
2025-11-20 12:11:41 +0700

    Уберем лишние комменты

---
+++ LB0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k; //Создаем переменную, которая будет в себе хранить квадрат числа
+            for (int i = 11; i <= 19; i++) 
+            {
+                k = Convert.ToInt32(Math.Pow(i, 2)); // Выполняем возведение в степень
+                Console.WriteLine($"Квадрат числа {i} равен {k}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
